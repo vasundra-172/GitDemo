2b86ce26831f3f41a49b6aae32acd141e85a2368
vasundra-172 <vishwakarmavasundra@gmail.com>
2024-05-04 21:04:49 +0530

    Modified after cloning

--- Launch.java
+++ Launch.java
@@ -4,4 +4,12 @@ class Launch
 	{
 		System.out.println("Exploring Git & Github");
 	}
+	public static void fun(String[] args) 
+	{	
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println(sum);
+	}
+
 }
